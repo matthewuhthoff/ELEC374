54192deda9e5bf5a34ea9a65b114ec7f85c2f449
Matthew Uhthoff <mattuhthoff@gmail.com>
2023-03-10 14:32:24 -0500

    changes

--- inc_PC.v
+++ inc_PC.v
@@ -1,19 +1,19 @@
 `timescale 1ns / 1ps
 
-module IncPC_32_bit #(parameter qInitial = 0)(
+module IncPC #(parameter qInit = 0)(
 	input clk, IncPC, enable,
 	input [31:0] inputPC,
-	output reg[31:0] newPC
+	output reg[31:0] tempPC
 	);
 	
-initial newPC = qInitial;
+initial tempPC = qInit;
 	
 always @ (posedge clk)
 	begin
 		if(IncPC == 1 && enable ==1)
-			newPC <= newPC + 1;
+			tempPC <= newPC + 1;
 		else if (enable == 1)
-			newPC <= inputPC;
+			tempPC <= inputPC;
 	end
 				
 endmodule
